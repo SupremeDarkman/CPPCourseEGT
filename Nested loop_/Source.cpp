34533dcc63f148d3e02fa75dba354fb0eebcb3a9
SupremeDarkman <64687911+SupremeDarkman@users.noreply.github.com>
2024-02-05 09:21:32 +0200

    Add files via upload

---
+++ Nested loop_/Source.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+const int CAPACITY = 10;
+
+
+
+int main()
+{	
+//	int numbers[CAPACITY][CAPACITY];
+//	long count = 0;
+//
+//
+//
+//	int rows, cols;
+//	cin >> rows >> cols;
+//
+//	for (int row = 0; row < rows; row++)
+//	{
+//		for (int col = 0; col < cols; col++)
+//		{
+//			cin >> numbers[row][col];
+//		}
+//	}
+//
+//	for (int row = 0; row < rows; row++)
+//	{
+//		for (int col = 0; col < cols; col++)
+//		{
+//			if (numbers[row][col] == 20)
+//			{
+//				cout << "Month is " << row + 1 << endl;
+//				cout << "Day is " << col + 1 << endl;
+//				count++;
+//			}
+//		}
+//	}
+//
+//	cout << "Temp 20 count " << count << endl; 
+
+
+	//const char* months[12] = { "Jan", "Feb" , "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep" , "Oct", "Nov", "Dec"};
+
+
+
+
+
+
+
+
+
+
+
+	//06.12.2023
+	//int n, m;
+	//std::cin >> n >> m;
+	//int t = 1;
+	//bool nameren = false;
+
+	//for (int i = 2; i < m; i++)
+	//{
+	//	for (int k = 0; k < n; k++)
+	//	{
+	//		t *= i;
+	//	}
+	//	if (t > m)
+	//	{
+	//		std::cout << i - 1 << std::endl;
+	//		nameren = true;
+	//		break;
+	//	}
+	//	t = 1;
+	//	if (nameren)
+	//	{
+	//		break;
+	//	}
+	//}
+	return 0;
+}
