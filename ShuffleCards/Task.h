6ebdc71b4b456848efab75ac86e843ad3507791d
SupremeDarkman <64687911+SupremeDarkman@users.noreply.github.com>
2024-02-15 11:30:17 +0200

    Delete ShuffleCards directory

--- ShuffleCards/Task.h
+++
@@ -1,9 +0,0 @@
-//Тесте карти:
-//- метдо, който разбъкрва картите в тестето
-//Тесте корти - class PlayingCard
-//			- type //A,K,Q,J,10,9,8,7,6,5,4,3,2
-//			- Suit // spades, diamonds, clubs, hearths
-// 
-//			  - class DeckOfCards
-//				- displayDeck
-//				- raffleShuffle - разделя тестето на две части и взима по една от всяка купчина
