b2a61e1c1b1d75632ef1e2c20cdf92921bf254ac
SupremeDarkman <64687911+SupremeDarkman@users.noreply.github.com>
2024-02-16 12:38:39 +0200

    Update Rectangle.h

--- CodingTasks_Polymorphism_Homework/Rectangle.h
+++ CodingTasks_Polymorphism_Homework/Rectangle.h
@@ -5,7 +5,7 @@ class Rectangle : public Shape
 public:
 	Rectangle(float width, float height, std::string colour) : width(width), height(height), Shape(colour){}
 	float area(){ return width * height;}
-	void draw() override { std::cout << "Drawing a " << colour << " rectangle" << std::endl; }
+	void draw() const override { std::cout << "Drawing a " << colour << " rectangle" << std::endl; }
 	~Rectangle() {}
 private:
 	float width;
