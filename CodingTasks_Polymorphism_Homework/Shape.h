c342fab092c0de3011065c5d08cd8e7c70e37d7d
SupremeDarkman <64687911+SupremeDarkman@users.noreply.github.com>
2024-02-16 12:38:28 +0200

    Update Shape.h

--- CodingTasks_Polymorphism_Homework/Shape.h
+++ CodingTasks_Polymorphism_Homework/Shape.h
@@ -5,7 +5,7 @@ public:
 	Shape(std::string colour)  { this->colour = colour; }
 
 	virtual float area() = 0;
-	virtual void draw() = 0;
+	virtual void draw() const = 0;
 	virtual ~Shape() {}
 
 protected:
