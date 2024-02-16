4939989be430a2bf6d8147a6862eaf8d1c106ed9
SupremeDarkman <64687911+SupremeDarkman@users.noreply.github.com>
2024-02-16 12:37:20 +0200

    Update Circle.h

--- CodingTasks_Polymorphism_Homework/Circle.h
+++ CodingTasks_Polymorphism_Homework/Circle.h
@@ -4,9 +4,9 @@ const float PI = 3.14;
 class Circle : public Shape
 {
 public:
-	Circle(float r) : r(r) {}
+	Circle(float r, std::string colour) : r(r), Shape(colour) {}
 	float area() { return r * r * PI; }
-	void draw() override { std::cout << "Drawing a " << Shape::colour << " circle" << std::endl; }
+	void draw() override { std::cout << "Drawing a " << colour << " circle" << std::endl; }
 	~Circle() {}
 private:
 	float r;
