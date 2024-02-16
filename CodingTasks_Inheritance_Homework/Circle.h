e9caf3f81ad3a1ddb00193bf67d4cb7714dab450
SupremeDarkman <64687911+SupremeDarkman@users.noreply.github.com>
2024-02-16 13:12:31 +0200

    Update Circle.h

--- CodingTasks_Inheritance_Homework/Circle.h
+++ CodingTasks_Inheritance_Homework/Circle.h
@@ -3,6 +3,7 @@
 class Circle : public Shape
 {
 public:
+	Circle(std::string colour) : Shape(colour) {}
 	void draw() const override
 	{
 		std::cout << "Drawing a " << Shape::colour << " circle" << std::endl;
