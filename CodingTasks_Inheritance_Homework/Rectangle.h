8330bdc7897e08e4932cb2b0aac348d7f734dc7d
SupremeDarkman <64687911+SupremeDarkman@users.noreply.github.com>
2024-02-16 13:12:05 +0200

    Update Rectangle.h

--- CodingTasks_Inheritance_Homework/Rectangle.h
+++ CodingTasks_Inheritance_Homework/Rectangle.h
@@ -4,6 +4,7 @@
 class Rectangle : public Shape
 {
 public:
+	Rectangle(std::string colour) : Shape(colour) {}
 	void draw() const override
 	{
 		std::cout << "Drawing a " << Shape::colour << " rectangle" << std::endl;
